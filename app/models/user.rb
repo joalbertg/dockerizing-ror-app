be98afa87c8f0f0e04100485e547512e2ae694ce
Joalbert <joalbertgonzalez@gmail.com>
2022-04-25 17:54:35 -0500

    [ADD]🚀 user scaffold
    - update rubocop rules
    - add users_controller
    - add user model
    - add users route
    - add migration and schema
    - add user factory
    - add user model tests
    - add user request tests
    - add user routing tests

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  validates :name, :last_name, :username, :age, presence: true
+  validates :age, comparison: { greater_than: 18 }
+end
