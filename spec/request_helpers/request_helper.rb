2933ac84f117b2bbdd4c08f99b1678c86c071c90
Joalbert <joalbertgonzalez@gmail.com>
2022-04-27 12:45:52 -0500

    [ADD]🚀 linter with github-actions
    - add `rubocop_linter_action.yml` in `.github/config`
    - add `build_with_linters.yml` workflow
    - add badge github-action in README

--- spec/request_helpers/request_helper.rb
+++ spec/request_helpers/request_helper.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 module RequestHelper
   def json_body
     @json_body ||= JSON.parse(response.body)
   end
 
   def request(http_method, endpoint, parameters = nil)
-    send(http_method, endpoint, params: parameters&.to_json || {}, headers: build_headers)
+    public_send(http_method, endpoint, params: parameters&.to_json || {}, headers: build_headers)
   end
 
   def build_headers
